3305960a9748c4be499f7e7fe0d471a4856b5b9d
study-hary-id <haryansyah.study.id@gmail.com>
2021-09-13 14:47:47 +0700

    Add HomePageHandler & NotFoundHandler

---
+++ handlers/handlers.go
@@ -0,0 +1,28 @@
+package handlers
+
+import (
+	"html/template"
+	"net/http"
+
+	"github.com/julienschmidt/httprouter"
+)
+
+// templates initialize available html template in templates/ directory.
+var templates = template.Must(template.ParseGlob("templates/*"))
+
+// HomePageHandler serves html and render home page for URL Shortener website.
+func HomePageHandler(w http.ResponseWriter, _ *http.Request, _ httprouter.Params) {
+	err := templates.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// NotFoundHandler serves html and render 404 Page Not Found to unknown routes.
+func NotFoundHandler(w http.ResponseWriter, _ *http.Request) {
+	w.WriteHeader(http.StatusNotFound)
+	err := templates.ExecuteTemplate(w, "404.html", nil)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
